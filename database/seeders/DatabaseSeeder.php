4e0f9f2251aa511e0d60e55229e8349d0f61d0d1
sezohessen <sezohessen2000@gmail.com>
2021-01-02 13:50:51 +0200

    Seeder Location

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(
-            [CountrySeeder::class],
-            [GovernorateSeeder::class],
-        );
+        $this->call(CountrySeeder::class);
+        $this->call(GovernorateSeeder::class);
+        $this->call(CitySeeder::class);
     }
 }
