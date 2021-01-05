9993ac37d58d3e56a519dc89766869c56d290771
sezohessen <sezohessen2000@gmail.com>
2021-01-05 05:10:26 +0200

    Car Database

---
+++ app/Models/car_badge.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class car_badge extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_01_05_025831_create_trendings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trendings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('car_id')->unsigned();
+            $table->foreign('car_id')
+            ->references('id')->on('cars')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+            $table->date('day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trendings');
+    }
+}
