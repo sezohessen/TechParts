47d797a66fc819d65fb13b63f18b784637d66374
MohamedAnwerEida <01200643888r@gmail.com>
2021-02-07 17:35:26 +0200

    fix active

--- resources/views/dashboard/Logs/btn/causer.blade.php
+++ resources/views/dashboard/Logs/btn/causer.blade.php
@@ -2,17 +2,21 @@
     <?php
         $model=$causer_type::where('id', $causer_id)->first();
     ?>
-    @if($causer_type=='App\Models\User')
-        <a href="{{route('dashboard.users.edit', ['user' => $model->id])}}" class="btn btn-sm btn-primary ">
-            <i class=" icon-nm text-primary-50 flaticon2-reply"></i>
-        </a>
-    @elseif($causer_type=='App\Models\Car')
-        <a href="{{route('dashboard.car.edit', ['car' => $model->id])}}" class="btn btn-sm btn-primary">
-            <i class=" icon-nm text-primary-50 flaticon2-reply"></i>
-        </a>
+    @if($model)
+        @if($causer_type=='App\Models\User')
+            <a href="{{route('dashboard.users.edit', ['user' => $model->id])}}" class="btn btn-sm btn-primary ">
+                <i class=" icon-nm text-primary-50 flaticon2-reply"></i>
+            </a>
+        @elseif($causer_type=='App\Models\Car')
+            <a href="{{route('dashboard.car.edit', ['car' => $model->id])}}" class="btn btn-sm btn-primary">
+                <i class=" icon-nm text-primary-50 flaticon2-reply"></i>
+            </a>
+        @else
+            <span class="label label-danger label-pill label-inline mr-2">@lang("Empty")</span>
+        @endif
     @else
-        <span class="label label-danger label-pill label-inline mr-2">@lang("Empty")</span>
+        <span class="label label-danger label-pill label-inline mr-2">@lang("deleted")</span>
     @endif
 @else
-    <span class="label label-danger label-pill label-inline mr-2">@lang("Undefined")</span>
+    <span class="label label-danger label-pill label-inline mr-2">@lang("Unknow resource")</span>
 @endif
